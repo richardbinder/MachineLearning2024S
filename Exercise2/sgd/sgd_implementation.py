cb120f6ffe122c542f08aeabf830e91252ca1fa5
Richard Binder <binder.richard@outlook.de>
2024-05-25 20:27:49 +0200

    Implement SGD

---
+++ Exercise2/sgd/sgd_implementation.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class LinearRegressionSGD:
+    def __init__(self, learning_rate=0.01, epochs=1000):
+        self.learning_rate = learning_rate
+        self.iterations = epochs
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # GD
+        for _ in range(self.iterations):
+            y_pred = np.dot(X, self.weights) + self.bias
+            residuals = y_pred - y
+
+            # gradient of RSS for each weight w is dw = 2 * x * (y_pred - y)
+            # for bias it is db = 2 * (y_pred - y)
+            # normalization by the number of samples
+            dW = (1 / n_samples) * 2 * np.dot(X.T, residuals)
+            db = (1 / n_samples) * 2 * np.sum(residuals)
+
+            self.weights -= self.learning_rate * dW
+            self.bias -= self.learning_rate * db
+
+    def predict(self, X):
+        return np.dot(X, self.weights) + self.bias
