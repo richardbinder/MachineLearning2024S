01c9fbb620418e0b1d0d8c45fde9e5f682e8a2c5
Victor <e11776826@student.tuwien.ac.at>
2024-07-20 19:28:01 +0200

    turn map c by 90 degrees so it is bottom to top

--- Exercise3/maps/map_c/map_c.py
+++ Exercise3/maps/map_c/map_c.py
@@ -8,25 +8,25 @@ START = 2
 FINISH = 3
  
 # Define an array of 30 rows and 60 columns for a wide racetrack
-track = np.full((30, 100), WALL)
+track = np.full((100, 30), WALL)
 
 # Define the cosine curve track
 x = np.linspace(0, 4 * np.pi, 100)  # x values from 0 to 4*pi
 y = (np.cos(x) * 10 + 15).astype(int)  # map cosine values to rows 0 to 29
 
 for i in range(98):
-    track[y[i+1] - 1:y[i+1] + 3, i+1] = TRACK  # make the track wider
+    track[i+1, y[i+1] - 2:y[i+1] + 4] = TRACK  # make the track wider
 
 # Define starting line
-track[23:27, 0] = START
+track[99, 22:28] = START
 
 # Define finishing line
-track[23:27, 99] = FINISH
+track[0, 22:28] = FINISH
 
 print(track)
 
 # Save track in npy format
-with open('track_c.npy', 'wb') as f:
+with open('./saved_tracks/track_c.npy', 'wb') as f:
     np.save(f, track)
     
 # Personalized color map
